756254b0a2141275051b21c58ad14699c5267666
Isaac Rodriguez <isaac_024@hotmail.com>
2026-06-22 14:06:24 -0600

    code: Main summary and introductory comprehension of the dataset

---
+++ main.R
@@ -0,0 +1,33 @@
+# =====================  Auxiliar ==========================
+# Limpiar env
+rm(list = ls())
+
+# Set WD (cambiar a ruta adecuada: ´Ctrl + Shift + H´) 
+setwd("~/Big Data/Exploracion_Datos/Proyecto")
+# ==========================================================
+
+# ==================== Librerías ===========================
+library(dplyr)
+library(tidyr)
+# ==========================================================
+
+# Carga Dataset
+datos <- read.csv("yt-recommend.csv")
+
+# Mostrar primeras y ultimas líneas
+head(datos)
+#
+tail(datos)
+
+# Tipos de datos de cada variable
+str(datos)
+# Resumen estadistico 
+summary(datos)
+
+# Ver NAs y Duplicados
+colSums(is.na(datos))   
+# 
+anyDuplicated(datos)
+
+# Sapply
+sapply(datos, class)
